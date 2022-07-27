b372ea48202950b7b2436417031b89c594737ccd
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-07-27 15:05:02 +0700

    set up moi truong

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+const app = express()
+try {
+  ;(async () => {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ontap')
+    console.log('connected db ')
+  })()
+} catch (error) {
+  console.log('fail to connect db')
+}
+app.use(express.json())
+app.use(cors())
+app.listen(process.env.PORT, () => {
+  console.log(`connected port ${process.env.PORT}`)
+})
