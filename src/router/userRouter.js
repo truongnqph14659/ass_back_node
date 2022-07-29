8a9cc54c2703fe2b2d58b80df69adb30e2a73401
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-07-30 00:50:11 +0700

    Delete userRouter.js

--- src/router/userRouter.js
+++
@@ -1,5 +0,0 @@
-import { signin, signup } from '../controller/userController'
-import express from 'express'
-const userRouter = express.Router()
-userRouter.route('/signup').post(signup)
-export default userRouter
