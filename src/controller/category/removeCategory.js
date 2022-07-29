ad1c63c1cf6adf2867c66024fce490e8e127e987
Trịnh Đình Thông <thongtdph16715@fpt.edu.vn>
2022-07-29 09:40:49 +0700

    build api delete a category

---
+++ src/controller/category/removeCategory.js
@@ -0,0 +1,11 @@
+export const remove = async (req, res) => {
+    try {
+        const id = req.params.id; 
+        const category = await Category.findOneAndDelete({_id: id}).exec();
+        res.json(category);
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không thêm được sản phẩm'
+        })
+    }
+}
