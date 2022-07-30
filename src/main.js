0689af1d4df16e752546f5aa3f03ccf827cedb4e
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-07-30 23:36:53 +0700

    connect db atlas

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-import routerProduct from './router/product';
+import routerProduct from './router/product'
+import product from './router/product'
+import category from './router/category'
 
 const app = express()
 app.use('/api', routerProduct)
 try {
   ;(async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/ontap')
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DATABASE}:${process.env.PASSWORD}@cluster0.wbb0p5v.mongodb.net/nodejs`
+    )
     console.log('connected db ')
   })()
 } catch (error) {
@@ -17,6 +21,8 @@ try {
 }
 app.use(express.json())
 app.use(cors())
+app.use('/api/', category)
+app.use('api/', product)
 app.listen(process.env.PORT, () => {
   console.log(`connected port ${process.env.PORT}`)
 })
