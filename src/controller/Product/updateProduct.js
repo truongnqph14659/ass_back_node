69acdca4699368b31cf26c3cb061fa19d96ff73f
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-07-30 07:00:12 +0700

    build API update a product #65

---
+++ src/controller/Product/updateProduct.js
@@ -0,0 +1,12 @@
+import Product from "../../model/product";
+
+export const update = async (req, res) => {
+    try {
+        const product = await Product.findOneAndUpdate({_id: req.params.id}, req.body, { new: true } )
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không thêm được sản phẩm'
+        })
+    }
+}
