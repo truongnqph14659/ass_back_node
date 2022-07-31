e7441f98d407258afc5a399f3bd17dfec93fbdc8
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-07-31 18:15:37 +0700

    hoan thanh chuc nang register

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { createHmac, randomBytes  } from crypto
+import { createHmac, randomBytes } from 'crypto'
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -23,15 +23,15 @@ const userSchema = new mongoose.Schema({
 userSchema.methods = {
   encryptpass: (password) => {
     const salt = randomBytes(10).toString('hex')
-    if(!password || !salt) return false
+    if (!password || !salt) return false
     return {
-        saltHex:salt,
-        passencode: createHmac('sha256',salt).update(password).digest('hex')
+      saltHex: salt,
+      passencode: createHmac('sha256', salt).update(password).digest('hex'),
     }
   },
-  authenticate: (password,secretkey)=>{
-    if(!password || !secretkey) return false
-    return createHmac('sha256',secretkey).update(password).digest('hex')
- }
+  authenticate: (password, secretkey) => {
+    if (!password || !secretkey) return false
+    return createHmac('sha256', secretkey).update(password).digest('hex')
+  },
 }
 export default mongoose.model('user', userSchema)
