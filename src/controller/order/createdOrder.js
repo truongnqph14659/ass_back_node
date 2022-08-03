cadef4da66ce2197e3ebb5998994a547771f1f1e
= <huynguyenz2002@gmail.com>
2022-08-03 15:20:53 +0700

    update order

--- src/controller/order/createdOrder.js
+++ src/controller/order/createdOrder.js
@@ -1,29 +1,39 @@
-import Order from '../../model/order'
+import Order from "../../model/order";
 
-export const CeatedOrder =async (req,res)=>{
-    try {
-        // console.log(req.body);
-        const data = await new Order(req.body).save();
-        res.json(data)
-    } catch (error) {
-        res.status(400).json(
-            { 
-                message: 'khong them duoc san pham '
-            }
-        )
-    }
-}
+export const CeatedOrder = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const data = await new Order(req.body).save();
+    res.json(data);
+  } catch (error) {
+    res.status(400).json({
+      message: "khong them duoc san pham ",
+    });
+  }
+};
 
 //list order
-export const List = async (req,res)=>{
-    try {
-        const data = await Order.find();
-        res.json(data)
-    } catch (error) {
-        res.status(400).json(
-            { 
-                message: 'khong them duoc san pham '
-            }
-        )
-    }
-}
+export const List = async (req, res) => {
+  try {
+    const data = await Order.find();
+    res.json(data);
+  } catch (error) {
+    res.status(400).json({
+      message: "khong them duoc san pham ",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const data = await Order.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+  } catch (error) {
+    res.status(400).json({
+        error: "không tìm thấy sản phẩm",
+      });
+  }
+};
